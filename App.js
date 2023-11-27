05d923ea696192e907acde81d52e851f8e78395b
Othmam-Al-Ani <142592326+Othmam-Al-Ani@users.noreply.github.com>
2023-11-27 12:30:21 +0100

    Nfc isntall on android aswell aswell as reader

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import React from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
-import NfcManager, {NfcTech} from 'react-native-nfc-manager';
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import NfcManager, { NfcTech } from "react-native-nfc-manager";
 
 // Pre-step, call this before any NFC operations
 NfcManager.start();
 
 function App() {
+  const [isPressed, setIsPressed] = useState(false);
+  const [info, setInfo] = useState(null);
+
+  const classID = "2997B6E4";
+
+  const handlePressIn = () => {
+    setIsPressed(true);
+  };
+
+  const handlePressOut = () => {
+    setIsPressed(false);
+  };
+
   async function readNdef() {
     try {
       // register for the NFC tag with NDEF in it
       await NfcManager.requestTechnology(NfcTech.Ndef);
       // the resolved tag object will contain `ndefMessage` property
       const tag = await NfcManager.getTag();
-      console.warn('Tag found', tag);
+      setInfo(tag);
+      if (tag.id == classID) {
+        console.warn("Welcome!", tag.id);
+      }
+      console.warn("Tag found", tag);
     } catch (ex) {
-      console.warn('Oops!', ex);
+      console.warn("Oops!", ex);
     } finally {
       // stop the nfc scanning
       NfcManager.cancelTechnologyRequest();
@@ -23,18 +40,48 @@ function App() {
 
   return (
     <View style={styles.wrapper}>
-      <TouchableOpacity onPress={readNdef}>
+      <TouchableOpacity
+        style={[Butt.wrapper, isPressed ? Butt.pressed : null]}
+        onPressIn={handlePressIn}
+        onPressOut={handlePressOut}
+        onPress={readNdef}
+      >
         <Text>Scan a Tag</Text>
       </TouchableOpacity>
+
+      {(info && info.id) === classID ? (
+        <View>
+          <Text>welcome to class :{classID}</Text>
+        </View>
+      ) : (
+        <View>
+          <Text>ID:{info && info.id}</Text>
+        </View>
+      )}
     </View>
   );
 }
+const Butt = StyleSheet.create({
+  wrapper: {
+    backgroundColor: "#ffffff",
+    padding: 10,
+    borderRadius: 5,
+    borderColor: "#000000",
+    marginBottom: 20,
+  },
+  pressed: {
+    backgroundColor: "#301934",
+
+    opacity: 0.5,
+  },
+});
 
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ffffff",
   },
 });
 
